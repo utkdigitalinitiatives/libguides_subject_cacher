d7385fd4a315b68927a22c92a1d3f6112a50456a
Mark Baggett <mbagget1@utk.edu>
2019-05-20 11:37:00 -0400

    Load api key from config vars.

--- app/generate_subject_templates.py
+++ app/generate_subject_templates.py
@@ -16,8 +16,8 @@ def scheduled_job():
         with open(f'templates/{subject}', 'w') as my_json:
             x = Subject(f"https://lgapi-us.libapps.com/1.1/assets?site_id=681&asset_types=10&expand="
                         f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
-                        f"&key={settings['api_key']}", subject,
-                        f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={settings["api_key"]}&expand=owner')
+                        f"&key={api_key}", subject,
+                        f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={api_key}&expand=owner')
             my_json.write(x.create_document())
 
 
--- app/views.py
+++ app/views.py
@@ -12,6 +12,6 @@ for subject in settings['subjects']:
     def route_subject():
         x = Subject(f"https://lgapi-us.libapps.com/1.1/assets?site_id=681&asset_types=10&expand="
                     f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
-                    f"&key={settings['api_key']}", subject,
-                    f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={settings["api_key"]}&expand=owner')
+                    f"&key={api_key}", subject,
+                    f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={api_key}&expand=owner')
         return x.create_document()
