03c71502f1a77facd980b01dfe3bfd907eaf81a8
Mark Baggett <mbagget1@utk.edu>
2019-05-20 15:33:11 -0400

    Read data in directly.

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from app import app
 import os
-from flask import jsonify, render_template
+from flask import jsonify
 
 api_key = os.environ['api-key']
 
 
 @app.route('/<page>', methods=['GET'])
 def route_subject(page):
-    return render_template(jsonify(page))
+    x = open(f'app/templates/{page}', 'r')
+    return jsonify(x.read())
+
