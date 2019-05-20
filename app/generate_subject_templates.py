63cf37683b76fdefd3f057b2782cbdc908374112
Mark Baggett <mbagget1@utk.edu>
2019-05-20 09:44:38 -0400

    Add generator.

---
+++ app/generate_subject_templates.py
@@ -0,0 +1,21 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import yaml
+from .libguides_request import Subject
+
+schedule = BlockingScheduler()
+
+
+@schedule.scheduled_job('cron', day_of_week='mon-sun', hour=18, minute=38)
+def scheduled_job():
+    print("Running Scheduled Job")
+    settings = yaml.load(open('../config.yml'), 'r')
+    for subject in settings['subjects']:
+        with open(f'templates/{subject}', 'w') as my_json:
+            x = Subject(f"https://lgapi-us.libapps.com/1.1/assets?site_id=681&asset_types=10&expand="
+                        f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
+                        f"&key={settings['api_key']}", subject,
+                        f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={settings["api_key"]}&expand=owner')
+            my_json.write(x.create_document())
+
+
+schedule.start()
