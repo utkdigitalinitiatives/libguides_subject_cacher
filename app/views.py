f868fef456e6e49f2f9c11e31ea04f3c12e4fcda
Mark Baggett <mbagget1@utk.edu>
2019-05-20 13:32:49 -0400

    Trying dynamically generating routes again.

--- app/views.py
+++ app/views.py
@@ -20,12 +20,14 @@ def index():
     return "Hello, World!"
 
 
-# for subject in settings['subjects']:
-#     @app.route(f"/{str(subject)}", methods=['GET'])
-#     def route_subject():
-#         x = Subject(f"https://lgapi-us.libapps.com/1.1/assets?site_id=681&asset_types=10&expand="
-#                     f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
-#                     f"&key={api_key}", subject,
-#                     f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={api_key}&expand=owner')
-#         return x.create_document()
+for subject in settings['subjects']:
+    @app.route(f"/{str(subject)}", methods=['GET'])
+    def route_subject():
+        print(f"Attempting to create route for {subject}.")
+        x = Subject(f"https://lgapi-us.libapps.com/1.1/assets?site_id=681&asset_types=10&expand="
+                    f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
+                    f"&key={api_key}", subject,
+                    f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={api_key}&expand=owner')
+        sys.stdout.flush()
+        return x.create_document()
 
