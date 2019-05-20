7fa0ba56e8bb4a206c62f3a7d2db35c603d3f629
Mark Baggett <mbagget1@utk.edu>
2019-05-20 15:09:55 -0400

    Write to app/templates.

--- app/generate_subject_templates.py
+++ app/generate_subject_templates.py
@@ -11,12 +11,12 @@ print(len(subjects))
 sys.stdout.flush()
 
 
-@schedule.scheduled_job('cron', day_of_week='mon-sun', hour=19, minute=8)
+@schedule.scheduled_job('cron', day_of_week='mon-sun', hour=19, minute=11)
 def scheduled_job():
     print("Running Scheduled Job")
     for subject in subjects:
         print(f'Generating JSON for {subject}')
-        with open(f'templates/{subject}', 'w') as my_json:
+        with open(f'app/templates/{subject}', 'w') as my_json:
             x = Subject(f"https://lgapi-us.libapps.com/1.1/assets?site_id=681&asset_types=10&expand="
                         f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
                         f"&key={api_key}", subject,
