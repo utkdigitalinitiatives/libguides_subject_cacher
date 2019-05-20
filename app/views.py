dc172ea62d8d2bc09eefec93d77bd644c052ddea
Mark Baggett <mbagget1@utk.edu>
2019-05-20 13:47:27 -0400

    Try dynamically loading again.

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ import yaml
 from .libguides_request import Subject
 import os
 import sys
+from flask import jsonify
 
 
 print("Starting Flask")
@@ -15,28 +16,28 @@ print("Creating Routes")
 sys.stdout.flush()
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return "Hello, World!"
+# @app.route('/', methods=['GET'])
+# def index():
+#     return "Hello, World!"
 
 
-# for subject in settings['subjects']:
-#     @app.route(f"/{str(subject)}", methods=['GET'])
-#     def route_subject():
-#         print(f"Attempting to create route for {subject}.")
-#         x = Subject(f"https://lgapi-us.libapps.com/1.1/assets?site_id=681&asset_types=10&expand="
-#                     f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
-#                     f"&key={api_key}", subject,
-#                     f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={api_key}&expand=owner')
-#         sys.stdout.flush()
-#         return x.create_document()
+for subject in settings['subjects']:
+    @app.route(f"/{str(subject)}", methods=['GET'])
+    def route_subject():
+        print(f"Attempting to create route for {subject}.")
+        x = Subject(f"https://lgapi-us.libapps.com/1.1/assets?site_id=681&asset_types=10&expand="
+                    f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
+                    f"&key={api_key}", subject,
+                    f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={api_key}&expand=owner')
+        sys.stdout.flush()
+        return jsonify(x.create_document())
 
-@app.route('/39797', methods=['GET'])
-def route_biology():
-    print(f"Attempting to create route for 39797.")
-    x = Subject(f"https://lgapi-us.libapps.com/1.1/assets?site_id=681&asset_types=10&expand="
-                        f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
-                        f"&key={api_key}", 39797,
-                        f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={api_key}&expand=owner')
-    sys.stdout.flush()
-    return x.create_document()
+# @app.route('/39797', methods=['GET'])
+# def route_biology():
+#     print(f"Attempting to create route for 39797.")
+#     x = Subject(f"https://lgapi-us.libapps.com/1.1/assets?site_id=681&asset_types=10&expand="
+#                         f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
+#                         f"&key={api_key}", 39797,
+#                         f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={api_key}&expand=owner')
+#     sys.stdout.flush()
+#     return jsonify(x.create_document())
