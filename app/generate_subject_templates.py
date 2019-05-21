6d3708ce23ff328aca7fc09d1d6f95c9fa931c68
Mark Baggett <mbagget1@utk.edu>
2019-05-21 15:50:36 -0400

    Manage time with config vars.

--- app/generate_subject_templates.py
+++ app/generate_subject_templates.py
@@ -3,7 +3,6 @@ from libguides_request import Subject
 import os
 import sys
 import boto3
-from boto3 import s3
 
 schedule = BlockingScheduler()
 api_key = os.environ['api-key']
@@ -11,10 +10,12 @@ subjects = os.environ['subjects'].split(',')
 bucket = os.environ['S3_bucket_name']
 secret_key = os.environ['S3_secret_key']
 access_key = os.environ['S3_access_key']
+hour = int(os.environ['hour'])
+minute = int(os.environ['minute'])
 s3_connection = boto3.resource('s3')
 
 
-@schedule.scheduled_job('cron', day_of_week='mon-sun', hour=19, minute=47)
+@schedule.scheduled_job('cron', day_of_week='mon-sun', hour=hour, minute=minute)
 def scheduled_job():
     print("Running Scheduled Job")
     for subject in subjects:
@@ -23,7 +24,7 @@ def scheduled_job():
                     f"permitted_uses,az_types,az_props,subjects,icons,friendly_url,permitted_uses"
                     f"&key={api_key}", subject,
                     f'http://lgapi-us.libapps.com/1.1/guides?site_id=681&key={api_key}&expand=owner')
-        object = s3.Object(bucket, subject)
+        object = s3_connection.Object(bucket, subject)
         object.put(Body=str.encode(x.create_document()))
         sys.stdout.flush()
     return
