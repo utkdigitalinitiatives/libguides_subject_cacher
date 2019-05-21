7497310d27e35468610c170fabbdbcdfba11fb14
Mark Baggett <mbagget1@utk.edu>
2019-05-21 08:57:52 -0400

    Load file with json loads.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 import os
 from flask import jsonify
+import json
 
 api_key = os.environ['api-key']
 
@@ -8,5 +9,5 @@ api_key = os.environ['api-key']
 @app.route('/<page>', methods=['GET'])
 def route_subject(page):
     x = open(f'app/templates/{page}', 'r')
-    return jsonify(x.read())
+    return jsonify(json.loads(x.read()))
 
