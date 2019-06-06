6c8aaf305e003714c71cf41b2fd3629dc19b78fc
Mark Baggett <mbagget1@utk.edu>
2019-06-06 11:24:10 -0400

    Add CORS.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 import sys
 
 app = Flask(__name__)
 app.config.from_object('config')
+CORS(app)
 
 print("Starting Flask")
 sys.stdout.flush()
